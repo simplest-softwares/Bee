dae8da64082d7fe679f16c3dfad5fe14adfd538f
5jd <85830207+5jd@users.noreply.github.com>
2023-01-04 14:04:32 +0100

    Updated Upstream (Purpur)
    Upstream has released updates that appear to apply and compile correctly
    
    Purpur Changes:
    PurpurMC/Purpur@9f5ba65 Updated Upstream (Paper)
    PurpurMC/Purpur@c3dea66 Updated Upstream (Paper)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import io.papermc.paperweight.util.constants.*
-
 plugins {
     java
     `maven-publish`
@@ -7,23 +5,9 @@ plugins {
     id("io.papermc.paperweight.patcher") version "1.4.0"
 }
 
-repositories {
-    mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
-        content {
-            onlyForConfigurations(PAPERCLIP_CONFIG)
-        }
-    }
-}
-
-dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
-    decompiler("net.minecraftforge:forgeflower:2.0.605.1")
-    paperclip("io.papermc:paperclip:3.0.2")
-}
-
-subprojects {
+allprojects {
     apply(plugin = "java")
+    apply(plugin = "maven-publish")
 
     java {
         toolchain {
@@ -32,33 +16,40 @@ subprojects {
     }
 }
 
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
+
 subprojects {
     tasks.withType<JavaCompile>().configureEach {
-        options.encoding = "UTF-8"
+        options.encoding = Charsets.UTF_8.name()
         options.release.set(17)
     }
-
     tasks.withType<Javadoc> {
         options.encoding = Charsets.UTF_8.name()
     }
-
     tasks.withType<ProcessResources> {
         filteringCharset = Charsets.UTF_8.name()
     }
 
     repositories {
-        mavenLocal()
         mavenCentral()
-        maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://papermc.io/repo/repository/maven-public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven(paperMavenPublicUrl)
         maven("https://jitpack.io")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
     }
+}
 
+repositories {
+    mavenCentral()
+    maven(paperMavenPublicUrl) {
+        content {
+            onlyForConfigurations(configurations.paperclip.name)
+        }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    decompiler("net.minecraftforge:forgeflower:2.0.605.1")
+    paperclip("io.papermc:paperclip:3.0.2")
 }
 
 paperweight {
@@ -80,5 +71,17 @@ paperweight {
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
             serverOutputDir.set(layout.projectDirectory.dir("Bee-Server"))
         }
+    }
+}
+
+tasks.register("printMinecraftVersion") {
+    doLast {
+        println(providers.gradleProperty("mcVersion").get().trim())
+    }
+}
+
+tasks.register("printPaperVersion") {
+    doLast {
+        println(project.version)
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,34 @@
 import java.util.Locale
 
+if (!file(".git").exists()) {
+    val errorText = """
+        
+        =====================[ ERROR ]=====================
+         The Bee project directory is not a properly cloned Git repository.
+         
+         In order to build Bee from source you must clone
+         the Purpur repository using Git, not download a code
+         zip from GitHub.
+         
+         Built Bee jars are available for download at
+         https://github.com/simplest-softwares/Bee/releases
+         
+         See Purpur's contributing guide (https://github.com/PurpurMC/Purpur/blob/HEAD/CONTRIBUTING.md)
+         for further information on building and modifying Bee.
+        ===================================================
+    """.trimIndent()
+    error(errorText)
+}
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
     }
 }
 
 rootProject.name = "bee"
+
 for (name in listOf("Bee-API", "Bee-Server")) {
     val projName = name.toLowerCase(Locale.ENGLISH)
     include(projName)
